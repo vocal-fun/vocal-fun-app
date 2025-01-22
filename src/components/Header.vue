b405fef0d4a4ceaeb1bda2fcfd59e2a602a47ba4
Stefan Popov <stef4n.popov@gmail.com>
2025-01-22 20:46:26 +0100

    Improve header styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,17 +2,41 @@
   <section class="header">
     <div class="socials">
       <a v-play-click-sound class="social shake-little" href="https://t.me/vocal_fun_official" target="_blank" rel="noopener noreferrer">
-        <img src="/img/telegram.png" alt="Telegram Logo" width="24" height="22">
+        <NuxtImg
+          class="telegram"
+          src="/img/telegram.png"
+          alt="Telegram Logo"
+          format="webp"
+          sizes="44px"
+          loading="lazy"
+        />
         TELEGRAM
       </a>
       <a v-play-click-sound class="social shake-little" href="https://x.com/vocalfun" target="_blank" rel="noopener noreferrer">
-        <img src="/img/twitter.png" alt="Twitter Logo" width="23" height="18">
+        <NuxtImg
+          class="twitter"
+          src="/img/twitter.png"
+          alt="Twitter Logo"
+          format="webp"
+          sizes="48px"
+          loading="lazy"
+        />
         TWITTER
       </a>
     </div>
 
     <button class="logo" @click="toggleBackgroundSound">
       <span class="logo-bar-element"></span>
+      <NuxtImg
+        class="shake-little shake-constant"
+        src="/logo/logo.png"
+        alt="Logo"
+        width="24"
+        height="24"
+        format="webp"
+        sizes="48px"
+        loading="lazy"
+      />
       vocal.fun
       <span class="logo-bar-element"></span>
     </button>
@@ -50,6 +74,7 @@ const toggleBackgroundSound = () => {
 
   .logo {
     cursor: pointer;
+    margin: 0.5rem;
     font-size: 1.2rem;
     display: inline-flex;
     align-items: center;
@@ -76,6 +101,7 @@ const toggleBackgroundSound = () => {
 
     a.social {
       display: inline-flex;
+      align-items: center;
       cursor: pointer;
       box-shadow: -3px 3px 0 0 #7B8494 inset,
                   -1.5px -1.5px 0 0 #4A5A6B inset,
@@ -88,12 +114,23 @@ const toggleBackgroundSound = () => {
 
       img {
         margin-right: .1875rem;
+        display: block;
+      }
+      .telegram {
+        width: 22px;
+        aspect-ratio: 29 / 25;
+      }
+      .twitter {
+        width: 24px;
+        aspect-ratio: 9 / 8;
+        object-fit: none;
       }
     }
   }
 
   .wallet-connect {
     text-align: center;
+    white-space: nowrap;
     a {
       cursor: pointer;
       transition: color 0.3s ease-in-out;
