af4199d755bfaf23de096347c9a4e07af7f04742
Stefan Popov <stef4n.popov@gmail.com>
2025-01-31 02:58:26 +0100

    Fix build issue

--- src/composables/useNotification.ts
+++ src/composables/useNotification.ts
@@ -1,12 +1,12 @@
-import { useToast, TYPE } from 'vue-toastification';
+import { useToast, type TYPE } from 'vue-toastification';
 
 export function useNotification() {
   const toast = useToast();
 
   return {
-    success: (content: string) => toast(content, { type: TYPE.SUCCESS }),
-    info: (content: string) => toast(content, { type: TYPE.INFO }),
-    error: (content: string) => toast(content, { type: TYPE.ERROR }),
-    warning: (content: string) => toast(content, { type: TYPE.WARNING }),
+    success: (content: string) => toast(content, { type: 'success' as TYPE }),
+    info: (content: string) => toast(content, { type: 'info' as TYPE }),
+    error: (content: string) => toast(content, { type: 'error' as TYPE }),
+    warning: (content: string) => toast(content, { type: 'warning' as TYPE }),
   };
 }
