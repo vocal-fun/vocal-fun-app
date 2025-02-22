b532ceb824e31ce5e75d058bc087c285f4e0ee71
Stefan Popov <stef4n.popov@gmail.com>
2025-02-22 16:24:13 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -114,16 +114,14 @@ export default defineNuxtConfig({
       "/api/v1/**": { proxy: "https://api.vocal.fun/api/v1/**" },
       "/api/image/**": { proxy: "https://vocal-fun.s3.ap-south-1.amazonaws.com/agents/**" },
     },
+    experimental: {
+      wasm: true,
+    },
   },
 
   vite: {
     optimizeDeps: {
       exclude: ['@ffmpeg/ffmpeg'],
-    },
-    build: {
-      rollupOptions: {
-        external: ['@ffmpeg/ffmpeg'],
-      },
     },
   },
 });
